3ce70c4736093afe9c697b980eb786d30dc87ccc
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-12 21:52:20 +0530

    Add a placeholder for youtube api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const KEY = 'YOUR_API_KEY';
+// ADD your API Key from https://console.developers.google.com to use the youtube API
+const KEY = 'YOUR_API_KEY_FROM_CONSOLE_GOOGLE_DEVELOPERS_DOT_COM_WEBSITE';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
