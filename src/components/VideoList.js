7b439eb1e243addcdd793dc245cce518d6543ce0
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-12 21:43:07 +0530

    Add styling to app.js

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,7 +2,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem key={video.id} video={video} onVideoSelect={onVideoSelect} />;
+    return <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
